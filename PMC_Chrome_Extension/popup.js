d8b4593383f1098874bb946990d4bb09d97b7aa7
Death Dev <DeathGameDev@gmail.com>
2016-04-14 06:43:24 -0500

    Made toggling the theme off 'stick'
    'Stick' = If you enable the theme, leave chat and come back, it'll still
    be on the theme and not require you to click it again.

--- PMC_Chrome_Extension/popup.js
+++ PMC_Chrome_Extension/popup.js
@@ -1,8 +1,82 @@
 
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse){
+        var msg = request.greeting;
+        if(msg.length > 1){
+            if(msg[0] == '*'){
+                //Getting what value the other script wants
+                var length = msg.length;
+                
+                var _key = mag.substring(1,length-1);
+                
+                sendMsg("*" + _key + "-" + getVal(_key));
+            }
+        }
+    }
+);
+
 document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById("clearThemeButton").addEventListener("click", clearTheme);
-    document.getElementById("addThemeButton").addEventListener("click", doTheme);
+    baseValCheck();
+    document.getElementById("toggleTheme").checked = getCurrentThemeToggleData_str();
+    document.getElementById("toggleTheme").addEventListener('change', toggleTheme);
 });
+
+function baseValCheck(){
+    //Essentially seeing if this is the first time we're running it.
+    if(getCurrentThemeToggleData() == undefined || getCurrentThemeToggleData() == null){
+        //console.log("undefined > baseValCheck");
+        setCurrentThemeToggleData('on');
+    }
+    
+    sendVal('themeToggleData');
+}
+
+function toggleTheme(){
+    //Getting the current theme data
+    var currData = $.jStorage.get('themeToggleData');
+    
+    //Let's see if it's on or off, or null D:
+    if(currData == null)
+        setVal('themeToggleData', 'on');
+    
+    if(currData == "on"){
+        sendMsg("clearTheme");
+        setVal('themeToggleData', 'off');
+        document.getElementById("toggleTheme").checked = getCurrentThemeToggleData_str();
+        //console.log("set toggleTheme to " + getCurrentThemeToggleData_str());
+    }else if(currData == "off"){
+        sendMsg("doTheme");
+        setVal('themeToggleData', 'on');
+        document.getElementById("toggleTheme").checked = getCurrentThemeToggleData_str();
+        //console.log("set toggleTheme to " + getCurrentThemeToggleData_str());
+    }
+}
+
+function getVal(key){
+    return $.jStorage.get(key);
+}
+
+function setVal(key, val){
+    doPreSetMsg(key,val);
+    $.jStorage.set(key, val);
+    doPostSetMsg(key,val);
+    sendVal(key, val);
+}
+
+function sendVal(key){
+    sendMsg("*" + key + "-" + getVal(key));
+}
+
+function getCurrentThemeToggleData(){
+    return getVal('themeToggleData');
+}
+function getCurrentThemeToggleData_str(){return (getCurrentThemeToggleData() == "on");}
+function setCurrentThemeToggleData(val){
+    setVal('themeToggleData', val);
+}
+
+function doPreSetMsg(key,val){console.log("[SET-PRE](" + key + ") " + getVal(key) + " > " + val);}
+function doPostSetMsg(key, val){console.log("[SET-POST](" + key + ")" + "Attempted to set to: " + val + ", actually set to " + getVal(key));}
 
 function clearTheme(){
     sendMsg("clearTheme");
@@ -15,7 +89,7 @@ function doTheme(){
 function sendMsg(txt){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {greeting: txt}, function(response) {
-            console.log("response.farewell");
+            
         });
     });
 }
--- PMC_Chrome_Extension/test.js
+++ PMC_Chrome_Extension/test.js
@@ -1,19 +1,45 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse){
-        var _type = request.greeting;
-        if(_type == "doTheme")
+        var msg = request.greeting;
+        if(msg == "doTheme")
             doTheme();
-        else if(_type == "clearTheme")
+        else if(msg == "clearTheme")
             clearTheme();
+        console.log("received msg in test.js, text = " + msg);
+        
+        if(msg.length > 1){
+            if(msg[0] == '*'){
+                //Example input:
+                //*KEY-VALUE
+                
+                //Getting the Key.
+                var _key = "";
+                for(var i = 1; i < msg.length; i++){
+                    if(msg[i] != '*' && msg[i] != '-')
+                        _key += msg[i];
+                    else if(msg[i] == '-')
+                        break;
+                }
+                
+                //Getting the Value.
+                var startingPos = 2 + _key.length;
+                var _value = msg.substring(startingPos, msg.length);
+                
+                //Setting the local variable
+                setVal(_key, _value);
+                //console.log("setting " + _key + " to " + _value);
+            }
+        }
     }
 );
 window.onload = function(){
-    
+    if(getVal('themeToggleData') == "on")
+        doTheme();
+    else
+        console.log("deemed themeToggleData incorrect for applying the theme, value was:" + getVal('themeToggleData'));
     document.getElementById("theme").onchange = function(){
         doTheme();  
     };
-    
-    doTheme();
 };
 
 function doTheme(){
@@ -27,10 +53,10 @@ function doTheme(){
     var opt = sel.selectedIndex;
     var isClassic = (sel.selectedIndex == 1);
     if(isClassic){
-        injectStyles('.chat_context #chatBottom > textarea {    background-image: none;    background-color: rgb(44, 62, 80);    color: rgb(236, 240, 241);}.site_btn {    background-image: none;    background-color: #2980b9;}#container {    background-image: none;    background-color: #0F161C;}body {    color: rgb(236, 240, 241);}.context-menu-item {    color: rgb(0,0,0);}.progresstext {    color: rgb(0,0,0);}#level_panel {    color: rgb(0,0,0);}#findDialog{color:rgb(0,0,0);}#blacklistDialog{color:rgb(0,0,0);}.description{color:rgb(0,0,0);}#reportsDialog{color:rgb(0,0,0);}.channel{color:rgb(255,255,255);}');
+        injectStyles('.chat_context #chatBottom > textarea {    background-image: none;    background-color: rgb(44, 62, 80);    color: rgb(236, 240, 241);}.site_btn {    background-image: none;    background-color: #2980b9;}#container {    background-image: none;    background-color: #0F161C;}body {    color: rgb(236, 240, 241);}.context-menu-item {    color: rgb(0,0,0);}.progresstext {    color: rgb(0,0,0);}#level_panel {    color: rgb(0,0,0);}#findDialog{color:rgb(0,0,0);}#blacklistDialog{color:rgb(0,0,0);}.description{color:rgb(0,0,0);}#reportsDialog{color:rgb(0,0,0);}.channel{color:rgb(255,255,255);}#rule_dialog{color:rgb(0,0,0);}');
         console.log("isClassic = true");
     }else{
-        injectStyles('.chat_context #chatBottom > textarea {    background-image: none;    background-color: rgb(44, 62, 80);    color: rgb(236, 240, 241);}.site_btn {    background-image: none;    background-color: #2980b9;}#container {    background-image: none;    background-color: #0F161C;}body {    color: rgb(0,0,0);}.context-menu-item {    color: rgb(0,0,0);}.progresstext {    color: rgb(0,0,0);}#level_panel {    color: rgb(0,0,0);}.userSpan.listName:not(.member_moderator):not(.bold){color:rgb(255,255,255)}.time{color:rgb(255,255,255)}#findDialog{color:rgb(0,0,0);}#blacklistDialog{color:rgb(0,0,0);}#reportsDialog{color:rgb(0,0,0);}label{color:rgb(255,255,255);}.channel{color:rgb(255,255,255);}');
+        injectStyles('.chat_context #chatBottom > textarea {    background-image: none;    background-color: rgb(44, 62, 80);    color: rgb(236, 240, 241);}.site_btn {    background-image: none;    background-color: #2980b9;}#container {    background-image: none;    background-color: #0F161C;}body {    color: rgb(0,0,0);}.context-menu-item {    color: rgb(0,0,0);}.progresstext {    color: rgb(0,0,0);}#level_panel {    color: rgb(0,0,0);}.userSpan.listName:not(.member_moderator):not(.bold){color:rgb(255,255,255)}.time{color:rgb(255,255,255)}#findDialog{color:rgb(0,0,0);}#blacklistDialog{color:rgb(0,0,0);}#reportsDialog{color:rgb(0,0,0);}label{color:rgb(255,255,255);}.channel{color:rgb(255,255,255);}#rule_dialog{color:rgb(0,0,0);}');
         console.log("isClassic = false");
     }
 }
@@ -48,13 +74,7 @@ function beginsWith(needle, haystack){
 
 
 function clearTheme(){
-    console.log("clearTheme called.");
-    var divs = document.getElementsByTagName("div");
-    for(var i = 0; i < divs.length; i++){
-        //console.log("CHECKING : " + divs[i].innerHTML);
-        if(divs[i].innerHTML.includes('­'))
-            divs[i].remove();
-    }
+    $("div:contains(­)").remove();
 }
 
 Element.prototype.remove = function() {
@@ -66,4 +86,25 @@ NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
             this[i].parentElement.removeChild(this[i]);
         }
     }
+}
+function getVal(key){
+    return $.jStorage.get(key);
+}
+
+function setVal(key, val){
+    //doPreSetMsg(key,val);
+    $.jStorage.set(key, val);
+    //doPostSetMsg(key,val);
+}
+
+function doPreSetMsg(key,val){console.log("[SET-PRE](" + key + ") " + getVal(key) + " > " + val);}
+function doPostSetMsg(key, val){console.log("[SET-POST](" + key + ")" + "Attempted to set to: " + val + ", actually set to " + getVal(key));}
+
+
+function sendMsg(txt){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {greeting: txt}, function(response) {
+            
+        });
+    });
 }
